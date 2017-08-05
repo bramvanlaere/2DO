b9d0390bd11f1871e4bef8a38a64ba88f1e781fa
bramvanlaere <r0625771@student.thomasmore.be>
2017-08-05 10:35:52 +0200

    code toegevoegd door GitHub verwijderd

--- login.php
+++ login.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 <?php
 if( isset($_GET['logout'] ) ){
     $logout = $_GET['logout'];
@@ -8,20 +6,6 @@ if( isset($_GET['logout'] ) ){
         session_destroy();
     }
 }
-=======
-<<<<<<< Updated upstream
-=======
-
-
-
-
-
-
-
->>>>>>> Stashed changes
-<?php
->>>>>>> origin/master
-
 if(!empty($_POST)){
     try
     {
@@ -37,26 +21,18 @@ if(!empty($_POST)){
     $query = $dbh->prepare( $sql );
     $query->execute( array( ':email'=>$email ) );
     $results = $query->fetchAll( PDO::FETCH_ASSOC );
-
     foreach( $results as $row ){
         if(password_verify($password, $row['password'])){
-<<<<<<< HEAD
             session_start();
             $_SESSION['user_id'] = $email;
             header("Location: profile.php");
-=======
-            header('Location:index.php');
->>>>>>> origin/master
         }
         else
         {
             header('Location: login.php');
         }
     }
-
 }
-
-
 ?><!doctype html>
 <html lang="en">
 <head>
@@ -65,65 +41,34 @@ if(!empty($_POST)){
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-</head>
-<body>
-<form name="loggin" method="post">
-
-    <fieldset class="fieldset_one">
-
-        <legend><a class= "logo" href="#">logo</a></legend>
-=======
->>>>>>> origin/master
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link rel="stylesheet" href="css/screen.css">
 </head>
 <body>
 <script src="js/bootstrap.min.js"></script>
 <div class="container">
-<form class="text-center"name="loggin" method="post">
+    <form class="text-center"name="loggin" method="post">
 
-    <fieldset class="fieldset_one">
+        <fieldset class="fieldset_one">
 
-        <legend><a class="logo" href="#">logo</a></legend>
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> origin/master
-        <div>
-            <label for="email">Username</label>
-            <input id="email" name="email" type="text" placeholder="Username">
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input id="password" type="password" name="password" id="password" placeholder="Password">
-        </div>
+            <legend><a class="logo" href="#">logo</a></legend>
+            <div>
+                <label for="email">Username</label>
+                <input id="email" name="email" type="text" placeholder="Username">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input id="password" type="password" name="password" id="password" placeholder="Password">
+            </div>
 
-    </fieldset>
+        </fieldset>
 
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-    <button type="submit" >Login</button>
-    <p>forgotten your password ?</p>
-    <p>Dont have an account? <a href="register.php">Sign up here!</a></p>
-    <div class="feedback"></div>
+        <button type="submit" class="btn btn-info" >Login</button>
+        <p class="account">Dont have an account? <a href="register.php">Sign up here!</a></p>
+        <div class="feedback"></div>
 
-</form>
-=======
->>>>>>> origin/master
-    <button type="submit" class="btn btn-info" >Login</button>
-    <p class="account">Dont have an account? <a href="register.php">Sign up here!</a></p>
-    <div class="feedback"></div>
-    
-    
 
-</form></div>
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> origin/master
+
+    </form></div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,104 +1,8 @@
 <?php
-<<<<<<< HEAD
-=======
-<<<<<<< Updated upstream
-/**
- * Created by PhpStorm.
- * User: bramvanlaere
- * Date: 25/07/17
- * Time: 13:07
- *
- */
-
-if(!empty($_POST)) {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    $options = [
-        'cost' =>12,
-
-    ];
-    $password = password_hash($password, PASSWORD_DEFAULT, $options);
-
-    try
-    {
-        $PDOconn = new PDO('mysql:host=localhost; dbname=2DO', 'root', '');
-        $PDOconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        return $PDOconn;
-    }
-    catch(PDOException $e)
-    {
-        echo "niet geslaagt om te connecteren met de databank". $e->getMessage();
-    }
-
-
-
-    $query = "insert into users (email, password) values ('".$email."','".$password."') ";
-    $statement = $pdoconn->prepare("SELECT * from users where email = :email and
-password = :password");
-
-    $statement->bindParam(':email', $email);
-    $statement->bindParam(':password', $password);
-    $statement->execute();
-    $res = $pdoconn->query($query);
-    if ($res != false) {
-        session_start();$_SESSION['email'] = $email;
-        header('Location: loggedin.php');
-
-    } else {
-
-        header('Location: registration.php');
-
-    }
-}
-
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-</head>
-<body>
-<form name="loggin" method="post">
-
-    <fieldset class="fieldset_one">
-
-        <legend><a class= "logo" href="#">logo</a></legend>
-        <div class="links" ><h1>REGISTER</h1></a></div>
-        <div>
-            <label for="email">Enter a Username</label>
-            <input id="email" name="email" type="text" placeholder="">
-        </div>
-        <div>
-            <label for="password">Enter a Password</label>
-            <input id="password" type="password" name="password" id="password" placeholder="">
-        </div>
-
-    </fieldset>
-
-
-    <button type="submit" >Login</button>
-
-
-</form>
-</body>
-</html>
-=======
->>>>>>> origin/master
-
-
 if( !empty( $_POST ) )
 {
-
     if( !empty( $_POST['username'] ) && !empty( $_POST['password'] )  ){
         $conn = new PDO('mysql:host=localhost;dbname=2DO', "root", "");
-
         $email = $_POST["username"];
         $options = [
             'cost'=>12,
@@ -110,46 +14,30 @@ if( !empty( $_POST ) )
         $sth->execute();
         if( $sth -> execute() )
         {
-<<<<<<< HEAD
             session_start();
             $_SESSION["user_id"] = $email;
-=======
-
-            session_start();
-            $_SESSION["user"] = $email;
->>>>>>> origin/master
             header("Location: login.php");
         }
         else
         {
             echo "<h1>u bent niet geregistreerd</h1>";
         }
-
     }
 }
-
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Register</title>
-<<<<<<< HEAD
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link rel="stylesheet" href="css/screen.css">
-=======
->>>>>>> origin/master
 
 </head>
 <body>
 <div class="container">
     <div class="form">
-<<<<<<< HEAD
         <h1>Sign up right here and never forget when to do what!</h1>
-=======
->>>>>>> origin/master
         <div class="logosp"></div>
         <form action="" method="post">
             <div class="username">
@@ -159,7 +47,6 @@ if( !empty( $_POST ) )
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password" placeholder="Your password">
             </div>
-<<<<<<< HEAD
             <button class="btn btn-info">
                 Register
             </button>
@@ -169,14 +56,3 @@ if( !empty( $_POST ) )
 </div>
 </body>
 </html>
-=======
-            <button>
-                Register
-            </button>
-        </form>
-    </div>
-</div>
-</body>
-</html>
->>>>>>> Stashed changes
->>>>>>> origin/master
